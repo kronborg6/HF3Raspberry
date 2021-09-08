97f1b8c4e86781ecc5a6188443ad4ea20e1618d8
Kronborg <mkronborg7@gmail.com>
2021-09-08 12:03:56 +0200

    0.0.1

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ import grovepi
 potentiometer = 2
 led = 5
 
-grovepi,pinMode(led, "OUTPUT")
+grovepi.pinMode(led,"OUTPUT")
 time.sleep(1)
 i = 0
 
@@ -12,7 +12,9 @@ while True:
     try:
         i = grovepi.analogRead(potentiometer)
         print(i)
+        if i > 100:
+            grovepi.analogWrite(led, i / 4)
 
-        grovepi.analogWrite(led,i/4)
+        grovepi.analogWrite(led,0)
     except IOError:
         print("Error")
